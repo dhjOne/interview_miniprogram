7322edc18cb1c0805edbe599ce7c8077c2345f25
丁好杰 <425388123@qq.com>
2026-05-14 00:29:26 +0800

    分类

--- config/dev.js
+++ config/dev.js
@@ -4,8 +4,8 @@ export default {
   env: 'develop',
   
   // API 基础配置
-  // baseUrl: 'http://localhost:9991',
-   baseUrl: 'https://providers-containers-tackle-leeds.trycloudflare.com',
+  baseUrl: 'http://localhost:9991',
+  //  baseUrl: 'https://providers-containers-tackle-leeds.trycloudflare.com',
   // baseUrl: 'https://175vq12004jn.vicp.fun',
   apiPrefix: '/api',
   
